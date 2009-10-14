e476fa88e2c7969997438701957b205b316d5466
Patricio Paez <pp@pp.com.mx>
2009-10-14 13:26:52 -0500

    + igualstart, igualend + rangocentro   inicio* -> igual*ant

--- ecuaciones.py
+++ ecuaciones.py
@@ -30,20 +30,22 @@ for linenumber, linea in enumerate( text.splitlines() ):
     salida = ''
     findelinea = linea
     long = len( linea )
-    inicio = 0
-    inicioantes = 0
+    igualendant = 0
+    igualstartant = 0
     derechastartanterior = 0
     derechaendanterior = 0
-    m = igual.search( linea, inicio )
-    while m and inicio < long:
-        if linea[ m.start()-1: m.start() ] != '=' and \
-              linea[ m.end() : m.end()+1 ] != '=':
+    m = igual.search( linea, igualendant )
+    while m and igualendant < long:
+        igualstart = m.start()
+        igualend = m.end()
+        if linea[ igualstart-1: igualstart ] != '=' and \
+              linea[ igualend : igualend+1 ] != '=':
             # the smaller of msigigual, msigsepar, endofline
             derechaends = []
-            msigigual = sigigual.search( linea, m.end() )
+            msigigual = sigigual.search( linea, igualend )
             if msigigual:
                 derechaends.append( msigigual.end() - 1 )
-            msigsepar = sigsepar.search( linea, m.end() )
+            msigsepar = sigsepar.search( linea, igualend )
             if msigsepar:
                 derechaends.append( msigsepar.end() )
             derechaends.append( len( linea ) )
@@ -51,30 +53,31 @@ for linenumber, linea in enumerate( text.splitlines() ):
             #print derechaends
             # the larger of mantigual, mantsepar, mantdospuntos, beginofline
             izquierdastarts = []
-            mantigual = antigual.search( linea, inicioantes, m.start() )
+            mantigual = antigual.search( linea, igualstartant, igualstart )
             if mantigual:
                 #print mantigual.group(), mantigual.start(), mantigual.end()
                 izquierdastarts.append( mantigual.start() + 1 )
-            mantsepar = antsepar.search( linea, inicio, m.start() )
+            mantsepar = antsepar.search( linea, igualendant, igualstart )
             if mantsepar:
                 #print mantsepar.group(), mantsepar.start(), mantsepar.end()
                 izquierdastarts.append( mantsepar.start() + 2 )
-            mantdospuntos = antdospuntos.search( linea, inicio, m.start() )
+            mantdospuntos = antdospuntos.search( linea, igualendant, igualstart )
             if mantdospuntos:
                 izquierdastarts.append( mantdospuntos.start() + 2 )
             izquierdastarts.append( 0 )
             izquierdastart = max( izquierdastarts )
             #print izquierdastarts
             #print izquierdastart, derechaend,
-            rangoizquierda = linea[ izquierdastart : m.start() ]
-            rangoderecha = linea[ m.end() : derechaend ] 
+            rangoizquierda = linea[ izquierdastart : igualstart ]
+            rangocentro = linea[ igualstart : igualend ]
+            rangoderecha = linea[ igualend : derechaend ] 
             if rangoizquierda.strip(): # i hay algo en la izquierda
                 # analiza( rangoizquierda, rangoderecha )
                 if not salida:
                     salida = '%2s %s' % ( linenumber, linea[ : izquierdastart ] ) 
                 # no repetir lado izquierdo
-                if derechastartanterior != izquierdastart or derechaendanterior != m.start():
-                    salida = salida + '<%s>' % ( linea[ izquierdastart : m.start() ] )
+                if derechastartanterior != izquierdastart or derechaendanterior != igualstart:
+                    salida = salida + '<%s>' % ( rangoizquierda )
                 # = y el lado derecho
                 n = ''; expresion = ''
                 try:
@@ -91,21 +94,21 @@ for linenumber, linea in enumerate( text.splitlines() ):
                 if not rangoderecha.strip():  # vacio
                     n = '0'
                 if n:
-                    salida = salida + '%s[%s]' % ( linea[ m.start() : m.end() ], 
-                                        linea[ m.end() : derechaend ] )
+                    salida = salida + '%s[%s]' % ( rangocentro, 
+                                        rangoderecha )
                 elif expresion:
-                    salida = salida + '%s{%s}' % ( linea[ m.start() : m.end() ], 
-                                        linea[ m.end() : derechaend ] )
+                    salida = salida + '%s{%s}' % ( rangocentro, 
+                                        rangoderecha )
                 else:
-                    salida = salida + '%s<%s>' % ( linea[ m.start() : m.end() ], 
-                                        linea[ m.end() : derechaend ] )
-                derechastartanterior = m.end()
+                    salida = salida + '%s<%s>' % ( rangocentro, 
+                                        rangoderecha )
+                derechastartanterior = igualend
                 derechaendanterior = derechaend
                 findelinea =  linea[ derechaend : ]
-        inicioantes = m.start()
-        inicio = m.end()
-        m = igual.search( linea, inicio )
+        igualstartant = igualstart
+        igualendant = igualend
+        m = igual.search( linea, igualendant )
     if salida:
         print salida + findelinea
-    elif findelinea or inicio == 0: 
+    elif findelinea or igualendant == 0: 
         print '%2s %s' % ( linenumber, findelinea )
