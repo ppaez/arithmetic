303d88e7c9e33da9b58f7e0baeff8c9bd1afdbc3
Patricio Paez <pp@pp.com.mx>
2009-10-16 17:24:38 -0500

    Bloque eliminado

--- ecuaciones.py
+++ ecuaciones.py
@@ -53,72 +53,71 @@ for linenumber, linea in enumerate( text.splitlines() ):
     mIgualAnt = re.search( '^', linea )
     mIgualAct = reIgual.search( linea, mIgualAnt.end() )
     while mIgualAct:
-        if True:
 
-            # the larger of mIgualAnt, mSeparIzq, mDospuntosIzq, beginofline
-            IzquierdaStarts = []
-            IzquierdaStarts.append( mIgualAnt.end() )
-            mSeparIzq = reSepar.search( linea, mIgualAnt.end(), mIgualAct.start() )
-            if mSeparIzq:
-                IzquierdaStarts.append( mSeparIzq.end() )
-            mDospuntosIzq = reDospuntosIzq.search( linea, mIgualAnt.end(), mIgualAct.start() )
-            if mDospuntosIzq:
-                IzquierdaStarts.append( mDospuntosIzq.end() )
-            mBeginOfLine = re.search( '^ *', linea )
-            IzquierdaStarts.append( mBeginOfLine.end() )
-            izquierdaActStart = max( IzquierdaStarts )
+        # the larger of mIgualAnt, mSeparIzq, mDospuntosIzq, beginofline
+        IzquierdaStarts = []
+        IzquierdaStarts.append( mIgualAnt.end() )
+        mSeparIzq = reSepar.search( linea, mIgualAnt.end(), mIgualAct.start() )
+        if mSeparIzq:
+            IzquierdaStarts.append( mSeparIzq.end() )
+        mDospuntosIzq = reDospuntosIzq.search( linea, mIgualAnt.end(), mIgualAct.start() )
+        if mDospuntosIzq:
+            IzquierdaStarts.append( mDospuntosIzq.end() )
+        mBeginOfLine = re.search( '^ *', linea )
+        IzquierdaStarts.append( mBeginOfLine.end() )
+        izquierdaActStart = max( IzquierdaStarts )
 
-            # the smaller of mIgualSig, mSeparDer, endofline
-            DerechaEnds = []
-            mIgualSig = reIgual.search( linea, mIgualAct.end() )
-            if mIgualSig:
-                DerechaEnds.append( mIgualSig.start() )
-            mSeparDer = reSepar.search( linea, mIgualAct.end() )
-            if mSeparDer:
-                DerechaEnds.append( mSeparDer.start() )
-            mEndOfLine = re.search( ' *$', linea )
-            DerechaEnds.append( mEndOfLine.start() )
-            DerechaActEnd = min( DerechaEnds )
+        # the smaller of mIgualSig, mSeparDer, endofline
+        DerechaEnds = []
+        mIgualSig = reIgual.search( linea, mIgualAct.end() )
+        if mIgualSig:
+            DerechaEnds.append( mIgualSig.start() )
+        mSeparDer = reSepar.search( linea, mIgualAct.end() )
+        if mSeparDer:
+            DerechaEnds.append( mSeparDer.start() )
+        mEndOfLine = re.search( ' *$', linea )
+        DerechaEnds.append( mEndOfLine.start() )
+        DerechaActEnd = min( DerechaEnds )
 
-            rangoizquierda = linea[ izquierdaActStart : mIgualAct.start() ]
-            rangocentro = linea[ mIgualAct.start() : mIgualAct.end() ]
-            rangoderecha = linea[ mIgualAct.end() : DerechaActEnd ] 
+        rangoizquierda = linea[ izquierdaActStart : mIgualAct.start() ]
+        rangocentro = linea[ mIgualAct.start() : mIgualAct.end() ]
+        rangoderecha = linea[ mIgualAct.end() : DerechaActEnd ] 
 
-            if rangoizquierda.strip(): # si hay algo en la izquierda
-                # analiza( rangoizquierda, rangoderecha )
-                if not salida:
-                    salida = '%2s ' % ( linenumber )
-                salida = salida + '%s' % ( linea[ DerechaAntEnd : izquierdaActStart ] )
+        if rangoizquierda.strip(): # si hay algo en la izquierda
+            # analiza( rangoizquierda, rangoderecha )
+            if not salida:
+                salida = '%2s ' % ( linenumber )
+            salida = salida + '%s' % ( linea[ DerechaAntEnd : izquierdaActStart ] )
 
-                tipoIzq, valorIzq = TipoValorDe( rangoizquierda )
-                tipoDer, valorDer = TipoValorDe( rangoderecha )
-                if tipoIzq == 'e' and tipoDer == 'v':
-                    try:
-                        rangoderecha = eval( valorIzq )
-                    except:
-                        pass
+            tipoIzq, valorIzq = TipoValorDe( rangoizquierda )
+            tipoDer, valorDer = TipoValorDe( rangoderecha )
+            if tipoIzq == 'e' and tipoDer == 'v':
+                try:
+                    rangoderecha = eval( valorIzq )
+                except:
+                    pass
 
-                # no repetir lado izquierdo
-                if DerechaAntStart != izquierdaActStart or DerechaAntEnd != mIgualAct.start():
-                    if tipoIzq in 'ifv':
-                        salida = salida + '[%s]' % ( rangoizquierda ) 
-                    elif tipoIzq == 'e':
-                        salida = salida + '{%s}' % ( rangoizquierda )
-                    else:
-                        salida = salida + '<%s>' % ( rangoizquierda )
-                # = y el lado derecho
-                if tipoDer in 'ifv':
-                    salida = salida + '%s[%s]' % ( rangocentro, 
-                                        rangoderecha )
-                elif tipoDer == 'e':
-                    salida = salida + '%s{%s}' % ( rangocentro, 
-                                        rangoderecha )
+            # no repetir lado izquierdo
+            if DerechaAntStart != izquierdaActStart or DerechaAntEnd != mIgualAct.start():
+                if tipoIzq in 'ifv':
+                    salida = salida + '[%s]' % ( rangoizquierda ) 
+                elif tipoIzq == 'e':
+                    salida = salida + '{%s}' % ( rangoizquierda )
                 else:
-                    salida = salida + '%s<%s>' % ( rangocentro, 
-                                        rangoderecha )
-                DerechaAntStart = mIgualAct.end()
-                DerechaAntEnd = DerechaActEnd
-                findelinea =  linea[ DerechaActEnd : ]
+                    salida = salida + '<%s>' % ( rangoizquierda )
+            # = y el lado derecho
+            if tipoDer in 'ifv':
+                salida = salida + '%s[%s]' % ( rangocentro, 
+                                    rangoderecha )
+            elif tipoDer == 'e':
+                salida = salida + '%s{%s}' % ( rangocentro, 
+                                    rangoderecha )
+            else:
+                salida = salida + '%s<%s>' % ( rangocentro, 
+                                    rangoderecha )
+            DerechaAntStart = mIgualAct.end()
+            DerechaAntEnd = DerechaActEnd
+            findelinea =  linea[ DerechaActEnd : ]
 
         mIgualAnt = mIgualAct
         mIgualAct = mIgualSig
