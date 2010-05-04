ccf2c1a204e954d06ed4db3ed0ef475066367c96
Patricio Paez <pp@pp.com.mx>
2010-05-04 12:39:23 -0500

    Uso de linea
    Eliminar construir salida
    Usar resultado y modificar

--- ecuaciones.py
+++ ecuaciones.py
@@ -36,8 +36,7 @@ def feed( text ):
     lines = []
 
     for linenumber, linea in enumerate( text.splitlines() ):
-        salida = ''
-        findelinea = linea
+
         DerechaAntStart = 0
         DerechaAntEnd = 0
         mIgualAnt = re.search( '^', linea )
@@ -81,50 +80,36 @@ def feed( text ):
 
             if tipoIzq != 'v': # si hay algo en la izquierda
 
-                # genera salida
-                salida = salida + '%s' % ( rangolibre )
-                # no repetir lado izquierdo
-                if DerechaAntStart != izquierdaActStart or DerechaAntEnd != mIgualAct.start():
-                    if tipoIzq in 'ifv':
-                        salida = salida + '%s' % ( rangoizquierda )
-                    elif tipoIzq == 'e':
-                        salida = salida + '%s' % ( rangoizquierda )
-                    else:
-                        salida = salida + '%s' % ( rangoizquierda )
-
-                salida = salida + rangocentro
-
                 # hacer operaciones
-                rangoderechaOri = rangoderecha
 
                 if tipoIzq == 'e' and tipoDer == 'v':    # evalua expresion
                     try:
-                        rangoderecha = str( eval( valorIzq, globales ) )
-                        salida = salida + '%s<-' % ( rangoderecha )
+                        resultado = str( eval( valorIzq, globales ) )
+                        linea = linea[ :mIgualAct.end() ] + resultado + linea[ DerechaActEnd: ]
                     except:
                         print 'eval error:', tipoIzq, valorIzq, tipoDer, valorDer
                 elif tipoIzq == 'n' and tipoDer == 'v' \
                         and valorIzq in globales.keys(): # evalua variable o funcion
                     try: 
-                        rangoderecha = str( eval( valorIzq + '()', globales ) )
-                        salida = salida + '%s<-' % ( rangoderecha )
+                        resultado = str( eval( valorIzq + '()', globales ) )
+                        linea = linea[ :mIgualAct.end() ] + resultado + linea[ DerechaActEnd: ]
                     except:
                         print 'eval error:', tipoIzq, valorIzq, tipoDer, valorDer
                 elif tipoIzq == 'n' and tipoDer in 'if':
                   if valorIzq not in funciones:          # asigna a variable
                     try:
                         exec valorIzq + ' = lambda : ' + str(valorDer) in globales
-                        salida = salida + '%s' % ( rangoderecha )
+
                     except:
                         print 'exec error:', tipoIzq, valorIzq, tipoDer, valorDer
                         raise
                   else:                                  # evalua funcion
                     try:
-                        rangoderecha = str( eval( valorIzq + '()', globales ) )
-                        if rangoderecha == rangoderechaOri:
-                            salida = salida + '%s' % ( rangoderecha )
+                        resultado = str( eval( valorIzq + '()', globales ) )
+                        if resultado == rangoderecha:
+                            linea = linea[ :mIgualAct.end() ] + resultado + linea[ DerechaActEnd: ]
                         else:
-                            salida = salida + '%s<-' % ( rangoderecha )
+                            linea = linea[ :mIgualAct.end() ] + resultado + '<-' + linea[ DerechaActEnd: ]
                     except:
                         print 'eval error:', tipoIzq, valorIzq, tipoDer, valorDer
                 elif tipoIzq == 'n' and tipoDer in 'e':  # define variable o funcion
@@ -132,7 +117,7 @@ def feed( text ):
                         exec valorIzq + ' = lambda : ' + str(valorDer) in globales
                         if '()' in valorDer and valorIzq not in funciones:
                             funciones.append( valorIzq )
-                        salida = salida + '%s' % ( rangoderecha )
+
                     except:
                         print 'exec error:', tipoIzq, valorIzq, tipoDer, valorDer
                         raise
@@ -141,21 +126,20 @@ def feed( text ):
                         exec valorIzq + ' = lambda : ' + str(valorDer) in globales
                         if '()' in valorDer and valorIzq not in funciones:
                             funciones.append( valorIzq )
-                        salida = salida + '%s' % ( rangoderecha )
+
                     except:
                         print 'exec error:', tipoIzq, valorIzq, tipoDer, valorDer
                         raise
-                else:
-                    salida = salida + '%s' % ( rangoderecha )
+
+
 
                 DerechaAntStart = mIgualAct.end()
                 DerechaAntEnd = DerechaActEnd
-                findelinea =  linea[ DerechaActEnd : ]
 
             mIgualAnt = mIgualAct
             mIgualAct = mIgualSig
 
-        lines.append( salida + findelinea )
+        lines.append( linea )
 
     return '\n'.join( lines )
 
