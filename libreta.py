7184b8cd2233cedf5b7da4fb10548cf1acd434e6
Patricio Paez <pp@pp.com.mx>
2010-07-11 07:45:46 -0500

    Mantener posición del cursor

--- libreta.py
+++ libreta.py
@@ -3,13 +3,16 @@ import ecuaciones
 
 def loadFile( event):
     'Load file content into the buffer.'
+
+    cursorPosition = texto.index( 'insert' )
     text = open( sys.argv[1] ).read()
     texto.delete( '1.0', Tkinter.END )
     texto.insert( "1.0", text)
+    texto.mark_set( 'insert', cursorPosition )
 
 def saveFile( event):
     'Save the text buffer contents.'
-    text = open( sys.argv[1] ), 'w' )
+    text = open( sys.argv[1], 'w' )
     text.write( texto.get( '1.0', Tkinter.END  ) )
 
 def Recalculate( event ):
@@ -17,10 +20,11 @@ def Recalculate( event ):
 
     Read text buffer, calculate, update the text buffer.'''
 
+    cursorPosition = texto.index( 'insert' )
     text = texto.get( '1.0', Tkinter.END )
     texto.delete( '1.0', Tkinter.END )
     texto.insert( Tkinter.END, ecuaciones.feed( text ) )
-    return "break"
+    texto.mark_set( 'insert', cursorPosition )
 
 import Tkinter
 root = Tkinter.Tk()
