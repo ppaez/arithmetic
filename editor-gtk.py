9e81327cd8072e1058bd88ed9fb09dab7e7f56c5
Patricio Paez <pp@pp.com.mx>
2011-02-11 22:54:50 -0600

    Simple GTK-based editor

---
+++ editor-gtk.py
@@ -0,0 +1,58 @@
+
+import gtk
+import arithmetic
+
+class Editor(object):
+    'A minimal editor'
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file('editor.ui')
+        self.builder.connect_signals(self)
+        self.textview = self.builder.get_object( 'textview1' )
+
+
+    def run(self):
+        try:
+            gtk.main()
+        except KeyboardInterrupt:
+            pass
+    
+    def quit(self):
+        gtk.main_quit()
+
+
+    def on_window1_delete_event(self, *args):
+        self.quit()
+
+    def on_window1_button_press_event(self, *args):
+        self.do_arithmetic()
+
+    def do_arithmetic(self):
+        'Perform arithmetic operations'
+
+        # get the buffer
+        buf = self.textview.get_buffer()
+
+        # get cursor position as offset
+        insert_iter = buf.get_iter_at_mark( buf.get_insert() )
+        offset = insert_iter.get_offset()
+
+        # get the text
+        txt = buf.get_text(buf.get_start_iter(),
+                           buf.get_end_iter())
+
+        # parse and return modified text
+        parser = arithmetic.Parser()
+        txt = parser.parse( txt )
+
+        # insert text back
+        buf.set_text( txt )
+
+        # set cursor position to previous offset
+        insert_iter = buf.get_iter_at_offset( offset )
+        buf.place_cursor( insert_iter )
+
+if __name__ == '__main__':
+        editor = Editor()
+        editor.run()
