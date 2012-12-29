4dc811c45b3cbc14c0d32be8add192afa84962eb
Patricio Paez <pp@pp.com.mx>
2012-12-28 20:30:51 -0600

    Coverage
    - Completed Lexer, Evaluate,
      TypeAndValueOf.
    
    - Started Parser

--- tests.py
+++ tests.py
@@ -15,12 +15,27 @@ class Lexer( unittest.TestCase):
         self.assertEqual( lexer.value, None )
         self.assertEqual( lexer.type, '' )
 
+    def test_gettoken_newline(self):
+        from arithmetic import Lexer
+        lexer = Lexer( ' \n ' )
+        lexer.gettoken()
+        self.assertEqual( lexer.value, '\n' )
+        self.assertEqual( lexer.type, 'r' )
+
     def test_gettoken_f(self):
         from arithmetic import Lexer
         lexer = Lexer( ' 5 ' )
         lexer.gettoken()
         self.assertEqual( lexer.value, '5' )
         self.assertEqual( lexer.type, 'f' )
+
+    def test_gettoken_x_enclosed(self):
+        from arithmetic import Lexer
+        lexer = Lexer( '5x3' )
+        lexer.gettoken()
+        lexer.gettoken()
+        self.assertEqual( lexer.value, 'x' )
+        self.assertEqual( lexer.type, 'x' )
 
     def test_gettoken_n(self):
         from arithmetic import Lexer
@@ -43,7 +58,49 @@ class Lexer( unittest.TestCase):
         self.assertEqual( lexer.value, '+' )
         self.assertEqual( lexer.type, 'o' )
 
+    def test_gettoken_invalid(self):
+        from arithmetic import Lexer
+        lexer = Lexer( ' @ ' )
+        lexer.gettoken()
+        self.assertEqual( lexer.value, '@***' )
+        self.assertEqual( lexer.type, 'u' )
+
 class Evaluate( unittest.TestCase):
+
+    def test_evaluate_factor(self):
+        from arithmetic import evaluate
+        res = evaluate('5*2')
+        self.assertEqual( res, '10' )
+
+    def test_evaluate_negative_factor(self):
+        from arithmetic import evaluate
+        res = evaluate('5*-2')
+        self.assertEqual( res, '-10' )
+
+    def test_evaluate_factor_percentage(self):
+        from arithmetic import evaluate
+        res = evaluate('50%')
+        self.assertEqual( res, '0.5' )
+
+    def test_evaluate_factor_open_parenthesis(self):
+        from arithmetic import evaluate
+        res = evaluate('(1)')
+        self.assertEqual( res, '1' )
+
+    def test_evaluate_factor_multiple_word_identifier(self):
+        from arithmetic import evaluate
+        res = evaluate('avg val', variables={'avg val':'5'})
+        self.assertEqual( res, '5' )
+
+    def test_evaluate_factor_name_undefined(self):
+        from arithmetic import evaluate
+        from decimal import InvalidOperation
+        self.assertRaises( InvalidOperation, evaluate, 'a' )
+
+    def test_evaluate_factors(self):
+        from arithmetic import evaluate
+        res = evaluate('5**2')
+        self.assertEqual( res, '25' )
 
     def test_evaluate_numeric(self):
         from arithmetic import evaluate
@@ -74,6 +131,66 @@ class Evaluate( unittest.TestCase):
         from arithmetic import evaluate
         res = evaluate('f', functions={'f': 'f+1'})
         self.assertEqual( res, '0' )
+
+
+class TypeAndValueOf(unittest.TestCase):
+
+    def test_v(self):
+        from arithmetic import TypeAndValueOf
+        res = TypeAndValueOf('')
+        self.assertEqual( res, ('v', ''))
+
+    def test_f(self):
+        from arithmetic import TypeAndValueOf
+        res = TypeAndValueOf('5.0')
+        self.assertEqual( res, ('f', '5.0'))
+
+    def test_n(self):
+        from arithmetic import TypeAndValueOf
+        res = TypeAndValueOf('name')
+        self.assertEqual( res, ('n', 'name'))
+
+    def test_e(self):
+        from arithmetic import TypeAndValueOf
+        res = TypeAndValueOf('name + 2')
+        self.assertEqual( res, ('e', 'name + 2'))
+
+    def test_a(self):
+        from arithmetic import TypeAndValueOf
+        res = TypeAndValueOf('2 + 2')
+        self.assertEqual( res, ('a', '2 + 2'))
+
+class Parser(unittest.TestCase):
+
+    def test_instance(self):
+        from arithmetic import Parser
+        self.assertIsInstance( Parser(), Parser )
+
+    def test_parse(self):
+        from arithmetic import Parser
+        parser = Parser()
+        self.assertEqual( parser.parse('2+2='), '2+2=4' )
+
+    def test_parseLine_mSeparLeft(self):
+        from arithmetic import Parser
+        parser = Parser()
+        lines = ['  a  2+2 =']
+        parser.parseLine(0, lines)
+        self.assertEqual( lines[0], '  a  2+2 =4' )
+
+    def test_parseLine_mColonLeft(self):
+        from arithmetic import Parser
+        parser = Parser()
+        lines = ['a: 2+2 =']
+        parser.parseLine(0, lines)
+        self.assertEqual( lines[0], 'a: 2+2 =4' )
+
+    def test_parseLine_mEqualSignNext(self):
+        from arithmetic import Parser
+        parser = Parser()
+        lines = ['a: 2+2 =   2x3=']
+        parser.parseLine(0, lines)
+        self.assertEqual( lines[0], 'a: 2+2 = 4  2x3=6' )
 
 if __name__ == '__main__':
     unittest.main()
