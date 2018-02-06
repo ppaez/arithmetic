39ada0308ecdd7014cdc10b0e5a3362ba9fe4171
Patricio Paez <pp@pp.com.mx>
2018-02-06 00:46:20 -0600

    Check the arithmetic module first

--- vim-plugin/install.sh
+++ vim-plugin/install.sh
@@ -20,6 +20,15 @@ else
     exit 1
 fi
 
+echo Check the Python arithmetic module
+pushd /tmp > /dev/null
+if python3 -u -c 'import arithmetic' > /dev/null 2>&1; then
+    echo Ok
+else
+    echo Missing. Run the command \"su -c 'python3 setup.py install'\" to install it
+fi
+popd > /dev/null
+
 echo Copy files
 cp -avf $SOURCE/arithmetic.vim $DEST_PLUGIN
 cp -avf $SOURCE/arithmetic.txt $SOURCE/tutorial.txt $DEST_DOC
@@ -33,12 +42,4 @@ else
     echo Failed
 fi
 
-echo Check the Python arithmetic module
-pushd /tmp > /dev/null
-if python3 -u -c 'import arithmetic' > /dev/null 2>&1; then
-    echo Ok
-else
-    echo Missing. Run the command \"su -c 'python3 setup.py install'\" to install it
-fi
-popd > /dev/null
 echo Finished ------------------------------
