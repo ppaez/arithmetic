802cb5fce7f1da355f0972d95cb22583590726bf
Patricio Paez <pp@pp.com.mx>
2018-02-06 00:44:12 -0600

    Check if Vim includes Python 3 support

--- vim-plugin/install.sh
+++ vim-plugin/install.sh
@@ -9,10 +9,22 @@ mkdir -p $DEST_PLUGIN
 mkdir -p $DEST_DOC
 mkdir -p $DEST_PYTHON3
 
-echo Installing the arithmetic plugin
+echo Installing the arithmetic plugin -------
+
+echo Check that Vim includes Python 3 support
+if vim --version | grep +python3 > /dev/null; then
+    echo Ok
+else
+    echo Missing. In Debian, run the command \"apt install vim-nox\"
+    echo or \"apt install vim-gtk3\" to include Python 3 support
+    exit 1
+fi
+
+echo Copy files
 cp -avf $SOURCE/arithmetic.vim $DEST_PLUGIN
 cp -avf $SOURCE/arithmetic.txt $SOURCE/tutorial.txt $DEST_DOC
 cp -avf $SOURCE/wrapper.py $DEST_PYTHON3
+echo Ok
 
 echo Prepare :help arithmetic
 if vim --not-a-term --noplugin --cmd ':helptags $HOME/.vim/doc' --cmd ':quit' > /dev/null; then
@@ -29,4 +41,4 @@ else
     echo Missing. Run the command \"su -c 'python3 setup.py install'\" to install it
 fi
 popd > /dev/null
-echo Done
+echo Finished ------------------------------
