50214e6fd64776a4ef7a7dbd48553238ab86b1c8
Patricio Paez <pp@pp.com.mx>
2018-02-06 00:29:03 -0600

    Improve the Vim plugin installer
    - List each step
    - Check if the Python arithmetic module is installed,
      instruct the user how to install if missing

--- vim-plugin/install.sh
+++ vim-plugin/install.sh
@@ -9,8 +9,24 @@ mkdir -p $DEST_PLUGIN
 mkdir -p $DEST_DOC
 mkdir -p $DEST_PYTHON3
 
+echo Installing the arithmetic plugin
 cp -avf $SOURCE/arithmetic.vim $DEST_PLUGIN
 cp -avf $SOURCE/arithmetic.txt $SOURCE/tutorial.txt $DEST_DOC
 cp -avf $SOURCE/wrapper.py $DEST_PYTHON3
 
-vim --noplugin --cmd ':helptags $HOME/.vim/doc' --cmd ':quit'
+echo Prepare :help arithmetic
+if vim --not-a-term --noplugin --cmd ':helptags $HOME/.vim/doc' --cmd ':quit' > /dev/null; then
+    echo Ok
+else
+    echo Failed
+fi
+
+echo Check the Python arithmetic module
+pushd /tmp > /dev/null
+if python3 -u -c 'import arithmetic' > /dev/null 2>&1; then
+    echo Ok
+else
+    echo Missing. Run the command \"su -c 'python3 setup.py install'\" to install it
+fi
+popd > /dev/null
+echo Done
